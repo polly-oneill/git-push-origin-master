c9f92c9ec19b884ec8908c61c09bdfeca983e5ad
Polly ONeill <polly.c.oneill@gmail.com>
2021-01-30 11:44:52 +0000

    completed validations for models

---
+++ app/models/cocktail.rb
@@ -0,0 +1,5 @@
+class Cocktail < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    has_many :doses, dependent: :delete_all
+    has_many :ingredients, through: :doses, dependent: :delete_all
+end
